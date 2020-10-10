6dc28b3d4ae042d25f26ae16ad82abc5ca6926ba
zhouyangnk <zhouyangnk@pku.edu.cn>
2020-10-10 21:37:21 +0800

    fix bug: paste buffer

--- scripts/paste.sh
+++ scripts/paste.sh
@@ -11,6 +11,7 @@ fi
 # remove newline if one sentence
 if [[ $(tmux show-buffer | hexdump | grep -cE '[[:space:]](0[ad]|0d0a)[[:space:]]?') -eq 1 ]]; then
   tmux show-buffer | tr -d '\n' | tmux load-buffer -
+  tmux paste-buffer
 fi
 
 if tmux show-buffer | hexdump | grep -qE '[[:space:]](0[ad]|0d0a)[[:space:]]?'; then
